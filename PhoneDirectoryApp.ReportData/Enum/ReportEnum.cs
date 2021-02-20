906819b02173096dd3f2071bc74a67dc5cc7fb19
ugurkaramanoglu <ugur4797508@gmail.com>
2021-02-20 23:56:35 +0300

    Report sercive created and Models added.

---
+++ PhoneDirectoryApp.ReportData/Enum/ReportEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneDirectoryApp.ReportData.Enum
+{
+    public enum ReportEnum
+    {
+        Bekliyor,
+        Tamamlandı
+    }
+}
---
+++ PhoneDirectoryApp.ReportData/Model/Report.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneDirectoryApp.ReportData.Model
+{
+   public  class Report
+    {
+        public Guid ID { get; set; }
+        public string ReportName { get; set; }
+        public string Location { get; set; }
+        public int RecordUserCount { get; set; }
+        public int RecordPhoneCount { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public ReportEnum status { get; set; }
+    }
+}
