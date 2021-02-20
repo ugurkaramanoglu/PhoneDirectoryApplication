f9834ad03fcc780031e4396c64e782e5152f207c
ugurkaramanoglu <ugur4797508@gmail.com>
2021-02-21 00:14:48 +0300

    Report service Core layer created and DTOs added.

---
+++ PhoneDirectoryApp.ReportCore/UserReportDto/UserReportDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneDirectoryApp.ReportCore.UserReportDto
+{
+    public class UserReportDTO
+    {
+        public string Address { get; set; }
+    }
+}
