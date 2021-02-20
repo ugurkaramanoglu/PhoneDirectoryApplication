85210f5774b41cb44c7d7c829a498e9a839b82fe
ugurkaramanoglu <ugur4797508@gmail.com>
2021-02-20 23:35:42 +0300

    User service Core layer created and DTOs added.

---
+++ PhoneDirectoryApp.Core/UserDTO/ContactInfoDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneDirectoryApp.Core.UserDTO
+{
+    public class ContactInfoDto
+    {
+        public int ID { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public Guid UserID { get; set; }
+    }
+}
---
+++ PhoneDirectoryApp.Core/UserDTO/UserDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneDirectoryApp.Core.UserDTO
+{
+    public class UserDto
+    {
+        public UserDto()
+        {
+            contactInfoList = new List<ContactInfoDto>();
+        }
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string CompanyName { get; set; }
+
+        public ICollection<ContactInfoDto> contactInfoList { get; set; }
+    }
+}
