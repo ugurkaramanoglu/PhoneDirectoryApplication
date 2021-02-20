3c5cea7cfc0b3e5d7eb7077c8afad96134fb58c1
ugurkaramanoglu <ugur4797508@gmail.com>
2021-02-20 23:18:01 +0300

    User service Data Access Layer Models created.

---
+++ PhoneDirectoryApp.DataAccess/Model/ContactInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneDirectoryApp.DataAccess.Model
+{
+   public  class ContactInfo
+    {
+        public int ID { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public Guid UserID { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ PhoneDirectoryApp.DataAccess/Model/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneDirectoryApp.DataAccess.Model
+{
+    public class User
+    {
+        public User()
+        {
+            contactInfo = new List<ContactInfo>();
+        }
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string CompanyName { get; set; }
+
+        public ICollection<ContactInfo> contactInfo { get; set; }
+    }
+}
