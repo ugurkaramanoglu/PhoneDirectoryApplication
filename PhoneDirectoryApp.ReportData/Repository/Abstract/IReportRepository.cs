f7dc4a7a15667746b1ae30d2db127a430cf9332d
ugurkaramanoglu <ugur4797508@gmail.com>
2021-02-21 00:12:26 +0300

    Report Service data layer repository pattern added.

---
+++ PhoneDirectoryApp.ReportData/Repository/Abstract/IReportRepository.cs
@@ -0,0 +1,17 @@
+﻿using PhoneDirectoryApp.ReportData.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneDirectoryApp.ReportData.Repository.Abstract
+{
+    public interface IReportRepository
+    {
+        Report ReportPost(Report report);
+        List<Report> GetListReport(string locationName);
+        Report GetReportItemById(Guid guid);
+        void ReportRemove(Report report);
+        List<Report> GetAllReport();
+
+    }
+}
